797b1675c8f157a8f052526c1e09d70497fbb5e8
Dmitriy Polyakovskiy <wot.dima@yandex.ru>
2024-06-19 00:57:11 +0500

    Edited JS

--- script.js
+++ script.js
@@ -22,13 +22,12 @@ function swiperCard() {
       init = true
       swiper = new Swiper('.swiper', {
         direction: 'horizontal',
-        slidesPerView: 1,
+        slidesPerView: 'auto',
         spaceBetween: 24,
         pagination: {
           el: '.swiper-pagination',
           clickable: true,
         },
-        freeMode: true,
       })
     }
   } else if (init) {
